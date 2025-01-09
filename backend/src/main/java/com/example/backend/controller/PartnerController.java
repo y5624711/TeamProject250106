62f4760684db2d8b2ab1371c012ea2c83947da69
Jin19121 <jinkim19121@gmail.com>
2025-01-09 18:31:13 +0900

    0109 김지민 feat: 협력사 backend 기초

--- backend/src/main/java/com/example/backend/controller/PartnerController.java
+++ backend/src/main/java/com/example/backend/controller/PartnerController.java
@@ -1,7 +1,13 @@
 package com.example.backend.controller;
 
-import org.springframework.stereotype.Controller;
+import com.example.backend.service.PartnerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/partner")
 public class PartnerController {
+    final PartnerService service;
 }
