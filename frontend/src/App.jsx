b0fd6ea7816126b3de98a5e271570574462d8dcd
Yun <y5624711@gmail.com>
2025-01-09 15:39:32 +0900

    0109 윤병관 feat.Navbar임시 sideBar 임시

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RootLayout } from "./page/root/RootLayout.jsx";
 import AuthenticationContext from "./context/AuthenticationProvider.jsx";
 import React from "react";
+import { Main } from "./page/main/Main.jsx";
+import { CommonCodeList } from "./page/commonCode/CommonCodeList.jsx";
 
 axios.interceptors.request.use(function (config) {
   const token = localStorage.getItem("token");
@@ -18,7 +20,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
-    children: [],
+    children: [
+      { index: true, element: <Main /> },
+      { path: "commonCode/list", element: <CommonCodeList /> },
+    ],
   },
 ]);
 
--- frontend/src/components/root/Navbar.jsx
+++ frontend/src/components/root/Navbar.jsx
@@ -1,69 +1,55 @@
 import { Box, Flex, Heading, Spacer, Text } from "@chakra-ui/react";
 import { useContext } from "react";
 import { AuthenticationContext } from "../../context/AuthenticationProvider.jsx";
+import { useNavigate } from "react-router-dom";
 
 function NavItem({ children, ...rest }) {
   return (
-    <Box h={"30px"} w={"110px"} placeItems={"center"} {...rest}>
-      {children}
-    </Box>
-  );
-}
-
-function TextItem({ children, ...rest }) {
-  return (
-    <Text
-      fontWeight={"bold"}
-      color={"gray.700"}
+    <Box
+      h={"40px"}
+      w={{ base: "90px", sm: "110px" }} // 반응형: 작은 화면에서는 더 좁게
+      display="flex" // Flexbox 활성화
+      justifyContent="center" // 가로 방향 가운데 정렬
+      alignItems="center" // 세로 방향 가운데 정렬
+      _hover={{ cursor: "pointer", bgColor: "whiteAlpha.700" }}
       {...rest}
-      _hover={{ color: "black", cursor: "pointer" }}
     >
-      {children}
-    </Text>
+      <Text fontWeight={"bold"} color={"gray.700"} whiteSpace={"nowrap"}>
+        {children}
+      </Text>
+    </Box>
   );
 }
 
 export function Navbar() {
   const { id, isAuthenticated, logout } = useContext(AuthenticationContext);
 
+  const navigate = useNavigate();
+
   return (
     <Box>
       <Flex gap={5} pt={5} w={"80%"} mx={"auto"}>
         <Heading>타이틀</Heading>
         <Spacer />
-        <Heading>유저 아이디</Heading>
-        <Heading>로그아웃</Heading>
+        <Text>유저 아이디</Text>
+        <Text>로그아웃</Text>
       </Flex>
       <Flex
-        bgColor={"blue.500"}
+        mb={-2}
+        bgColor={"lightSlateGray"}
         alignItems="center"
         justifyContent="center"
-        gap={5}
+        flexWrap="wrap" // 줄바꿈 활성화
+        overflow="hidden" // 넘치는 요소를 숨김
       >
-        <NavItem>
-          <TextItem>가맹점 관리</TextItem>
-        </NavItem>
-        <NavItem>
-          <TextItem>협력업체 관리</TextItem>
-        </NavItem>
-        <NavItem>
-          <TextItem>물품 관리</TextItem>
-        </NavItem>
-        <NavItem>
-          <TextItem>설치 관리</TextItem>
-        </NavItem>
-        <NavItem>
-          <TextItem>공통코드</TextItem>
-        </NavItem>
-        <NavItem>
-          <TextItem>조회</TextItem>
-        </NavItem>
-        <NavItem>
-          <TextItem>인사관리</TextItem>
-        </NavItem>
-        <NavItem>
-          <TextItem>회원관리</TextItem>
-        </NavItem>
+        <NavItem>가맹점 관리</NavItem>
+        <NavItem>협력업체 관리</NavItem>
+        <NavItem>물품 관리</NavItem>
+        <NavItem>설치 관리</NavItem>
+        <NavItem onClick={() => navigate("/commonCode/list")}>공통코드</NavItem>
+        <NavItem>조회</NavItem>
+        <NavItem>인사관리</NavItem>
+        <NavItem>회원관리</NavItem>
       </Flex>
     </Box>
   );
