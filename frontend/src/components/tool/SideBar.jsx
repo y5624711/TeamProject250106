972ab32dc15cce012d870d292facd1aac4d2cd44
soomni01 <dsgh8933@naver.com>
2025-01-14 19:05:00 +0900

    0114 김수민 refactor: 사이드바 수정

--- frontend/src/components/tool/SideBar.jsx
+++ frontend/src/components/tool/SideBar.jsx
@@ -1,27 +1,8 @@
 import { Box, Flex, Stack, Text } from "@chakra-ui/react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-export function SideBar({ title, children }) {
+export function SideBar() {
   const navigate = useNavigate();
-  const location = useLocation();
-
-  // Sidebar 항목을 경로별로 정의
-  const sidebarItemsByPath = {
-    "/commonCode": [
-      { label: "공통 코드 목록", path: "/commonCode/" },
-      { label: "공통 코드 등록", path: "/commonCode/add" },
-    ],
-    "/otherPage": [
-      { label: "다른 메뉴 1", path: "/otherPage/menu1" },
-      { label: "다른 메뉴 2", path: "/otherPage/menu2" },
-    ],
-  };
-
-  // 현재 경로에 맞는 Sidebar 항목 선택
-  const currentPath = location.pathname.split("/")[1] || "/";
-  console.log(currentPath);
-  const sidebarItems =
-    sidebarItemsByPath[`/${currentPath}`] || sidebarItemsByPath["/"];
 
   return (
     <Flex>
@@ -41,29 +22,24 @@ export function SideBar({ title, children }) {
           alignItems="center" // 세로 방향 가운데 정렬
         >
           <Text fontWeight="bold" color={"white"} fontSize={"25px"}>
-            {title}
+            기준정보 관리
           </Text>
         </Box>
 
         <Stack spacing="2">
-          {sidebarItems.map((item, index) => (
-            <Box pb={2} pt={4} key={index}>
-              <Text
-                as="button"
-                onClick={() => navigate(item.path)}
-                _hover={{ cursor: "pointer" }}
-                textAlign="left"
-              >
-                {item.label}
-              </Text>
-            </Box>
-          ))}
+          <Text>사업장/부서 관리</Text>
+          <Text>인사 관리</Text>
+          <Text>가맹점 관리</Text>
+          <Text>협력업체 관리</Text>
+          <Text
+            onClick={() => navigate("/item")}
+            _hover={{ cursor: "pointer" }}
+          >
+            품목 관리
+          </Text>
+          <Text>창고 관리</Text>
+          <Text>로케이션 관리</Text>
         </Stack>
-      </Box>
-
-      {/*메인 컨텐츠 영역*/}
-      <Box flex="1" p={4}>
-        {children}
       </Box>
     </Flex>
   );
