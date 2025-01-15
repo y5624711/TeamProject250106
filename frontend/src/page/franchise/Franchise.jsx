503c5d89ccc968787f436b4eff26c6de776504ca
keaimk <fly1043@naver.com>
2025-01-15 17:11:49 +0900

    0115 김민경 feat : 가맹점 목록을 가져와서 오른쪽에 첫 번째를 기본값으로 띄움

--- frontend/src/page/franchise/Franchise.jsx
+++ frontend/src/page/franchise/Franchise.jsx
@@ -1,13 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Button, Heading } from "@chakra-ui/react";
 import { FranchiseList } from "../../components/franchise/FranchiseList.jsx";
 import { FranchiseView } from "../../components/franchise/FranchiseView.jsx";
 import { FranchiseAdd } from "../../components/franchise/FranchiseAdd.jsx";
 import { SideBar } from "../../components/tool/SideBar.jsx";
+import axios from "axios";
 
 export function Franchise() {
   const [viewMode, setViewMode] = useState("view");
   const [selectedFranchiseKey, setSelectedFranchiseKey] = useState(null);
+
+  // 가맹점 목록을 가져와서 첫 번째 가맹점의 franchiseKey를 기본값으로 설정
+  useEffect(() => {
+    axios.get("/api/franchise/list").then((response) => {
+      if (response.data.length > 0) {
+        setSelectedFranchiseKey(response.data[0].franchiseKey);
+      }
+    });
+  }, []);
 
   // 가맹점 클릭 시 선택된 franchiseKey를 상태로 설정
   const handleFranchiseClick = (franchiseKey) => {
