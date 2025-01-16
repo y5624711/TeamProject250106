c9faa7036f8e7764efeb1c295d13ce85a68501d2
soomni01 <dsgh8933@naver.com>
2025-01-16 20:16:00 +0900

    0116 김수민 refactor: 공통 코드 관리 navbar에서 제거

--- frontend/src/components/root/Navbar.jsx
+++ frontend/src/components/root/Navbar.jsx
@@ -34,9 +34,6 @@ export function Navbar() {
       <Flex gap={5} pt={5} w={"80%"} mx={"auto"}>
         <Heading>Choongang System</Heading>
         <Spacer />
-        <Text onClick={() => navigate("/commonCode/item/list")}>
-          공통 코드 관리
-        </Text>
         <Text>유저 아이디</Text>
         <Text>로그아웃</Text>
       </Flex>
