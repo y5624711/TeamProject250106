55fbf6a104a3d64da376b18a5e508b6e99270b19
keaimk <fly1043@naver.com>
2025-01-16 09:44:06 +0900

    0116 김민경 fix : 오른쪽에 첫 번째 가맹점 보여주기

--- frontend/src/page/franchise/Franchise.jsx
+++ frontend/src/page/franchise/Franchise.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box, Button, Heading } from "@chakra-ui/react";
+import { Box, Button, Heading, Spinner } from "@chakra-ui/react";
 import { FranchiseList } from "../../components/franchise/FranchiseList.jsx";
 import { FranchiseView } from "../../components/franchise/FranchiseView.jsx";
 import { FranchiseAdd } from "../../components/franchise/FranchiseAdd.jsx";
@@ -9,14 +9,23 @@ import axios from "axios";
 export function Franchise() {
   const [viewMode, setViewMode] = useState("view");
   const [selectedFranchiseKey, setSelectedFranchiseKey] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   // 가맹점 목록을 가져와서 첫 번째 가맹점의 franchiseKey를 기본값으로 설정
   useEffect(() => {
-    axios.get("/api/franchise/list").then((response) => {
-      if (response.data.length > 0) {
-        setSelectedFranchiseKey(response.data[0].franchiseKey);
-      }
-    });
+    axios
+      .get("/api/franchise/list")
+      .then((response) => {
+        if (response.data.list && response.data.list.length > 0) {
+          setSelectedFranchiseKey(response.data.list[0].franchiseKey); // 첫 번째 가맹점 선택
+        }
+      })
+      .catch((error) => {
+        console.error("가맹점 데이터를 불러오는 데 실패했습니다:", error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
   // 가맹점 클릭 시 선택된 franchiseKey를 상태로 설정
@@ -39,18 +48,24 @@ export function Franchise() {
           <Button colorScheme="teal" onClick={() => setViewMode("add")} mb={4}>
             추가
           </Button>
-          {/* viewMode에 따라 컴포넌트 변경 */}
-          {viewMode === "add" && (
-            <FranchiseAdd
-              setViewMode={setViewMode} // 상태 변경 함수 전달
-              setSelectedFranchiseKey={setSelectedFranchiseKey} // 선택된 가맹점 키 설정 함수 전달
-            />
-          )}
-          {viewMode === "view" && selectedFranchiseKey && (
-            <FranchiseView
-              franchiseKey={selectedFranchiseKey}
-              setViewMode={setViewMode} // 상태 변경 함수 전달
-            />
+          {/* 로딩 상태에 따른 조건부 렌더링 */}
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <>
+              {viewMode === "add" && (
+                <FranchiseAdd
+                  setViewMode={setViewMode} // 상태 변경 함수 전달
+                  setSelectedFranchiseKey={setSelectedFranchiseKey} // 선택된 가맹점 키 설정 함수 전달
+                />
+              )}
+              {viewMode === "view" && selectedFranchiseKey && (
+                <FranchiseView
+                  franchiseKey={selectedFranchiseKey}
+                  setViewMode={setViewMode} // 상태 변경 함수 전달
+                />
+              )}
+            </>
           )}
         </Box>
       </Box>
