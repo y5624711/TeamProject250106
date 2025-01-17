a46ce477ca03283d4d3cdf5ec250f3254ffd6d73
soomni01 <dsgh8933@naver.com>
2025-01-17 09:59:34 +0900

    0117 김수민 fix: 페이지네이션 새로고침 시 1페이지로 보이는 오류 해결

--- frontend/src/components/tool/Pagination.jsx
+++ frontend/src/components/tool/Pagination.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Center, HStack } from "@chakra-ui/react";
 import {
   PaginationItems,
@@ -6,9 +6,17 @@ import {
   PaginationPrevTrigger,
   PaginationRoot,
 } from "../ui/pagination.jsx";
+import { useSearchParams } from "react-router-dom";
 
 export function Pagination({ count, pageSize, onPageChange }) {
+  const [searchParams] = useSearchParams();
   const [currentPage, setCurrentPage] = useState(1);
+
+  // 새로고침 시 UI 표시가 1페이지로 초기화되는 오류 -> URL의 page 파라미터로 currentPage 초기화
+  useEffect(() => {
+    const page = parseInt(searchParams.get("page") || "1", 10); // 10진수로 변환
+    setCurrentPage(page);
+  }, [searchParams]);
 
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
