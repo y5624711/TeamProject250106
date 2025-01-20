46efb4a7caaaecd53ffe80c45f0da3ff61955b58
soomni01 <dsgh8933@naver.com>
2025-01-20 17:33:21 +0900

    0120 김수민 feat: 설치 백엔드 구성

---
+++ backend/src/main/java/com/example/backend/dto/install/Install.java
@@ -0,0 +1,8 @@
+package com.example.backend.dto.install;
+
+import lombok.Data;
+
+@Data
+public class Install {
+
+}
---
+++ backend/src/main/java/com/example/backend/mapper/install/InstallMapper.java
@@ -0,0 +1,7 @@
+package com.example.backend.mapper.install;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface InstallMapper {
+}
