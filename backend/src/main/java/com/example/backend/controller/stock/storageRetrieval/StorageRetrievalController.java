1a6d2979d6199b8a336eb3adc293010deaa373cb
Lemonstew <colt4949@naver.com>
2025-01-21 18:18:53 +0900

    0121 민재원 feat: 물품입출내역 백엔드 생성

--- backend/src/main/java/com/example/backend/controller/stock/storageRetrieval/StorageRetrievalController.java
+++ backend/src/main/java/com/example/backend/controller/stock/storageRetrieval/StorageRetrievalController.java
@@ -1,11 +1,26 @@
 package com.example.backend.controller.stock.storageRetrieval;
 
+import com.example.backend.dto.stock.storageRetrieval.StorageRetrieval;
+import com.example.backend.service.stock.storageRetrieval.StorageRetrievalService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/storageRetrieval")
 @RequiredArgsConstructor
 public class StorageRetrievalController {
+
+    final StorageRetrievalService service;
+
+    @GetMapping("list")
+    public Map<String, Object> list() {
+        return service.list();
+    }
+
+    @PostMapping("add")
+    public void add(@RequestBody StorageRetrieval storageRetrieval) {
+        service.add(storageRetrieval);
+    }
 }
