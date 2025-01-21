2ce5754f8e845335e9d27a31e73af79e70e10a5e
keaimk <fly1043@naver.com>
2025-01-21 14:49:17 +0900

    0121 김민경 feat : 구매 요청/승인 sql 생성

---
+++ sql/PurchaseRequest.sql
@@ -0,0 +1,12 @@
+CREATE TABLE TB_PURCH_REQ
+(
+    purchase_request_key  INT AUTO_INCREMENT PRIMARY KEY,
+    item_common_code      VARCHAR(5)  NOT NULL,
+    business_employee_no  VARCHAR(13) NOT NULL,
+    amount                INT         NOT NULL,
+    purchase_request_date DATE        DEFAULT CURRENT_DATE,
+    purchase_consent      BOOLEAN     DEFAULT NULL,
+    purchase_request_note VARCHAR(50) DEFAULT NULL
+);
+
+DROP TABLE TB_PURCH_REQ;
