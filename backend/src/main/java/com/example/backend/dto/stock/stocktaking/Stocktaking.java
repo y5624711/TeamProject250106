5c24290b52cc31889db373064e6358889ea5234f
Lemonstew <colt4949@naver.com>
2025-01-23 14:02:16 +0900

    0123 민재원 feat: 재고실사 데이터베이스 작업중

--- backend/src/main/java/com/example/backend/dto/stock/stocktaking/Stocktaking.java
+++ backend/src/main/java/com/example/backend/dto/stock/stocktaking/Stocktaking.java
@@ -19,12 +19,16 @@ public class Stocktaking {
     private String warehouseName;
     //    품목명
     private String itemName;
+    //    협력업체명
+    private String customerName;
+    //    협력업체 코드
+    private String customerCode;
+    //    업체 전화번호
+    private String customerTel;
     //    협력사 직원 이름
     private String customerEmployeeName;
     //    전화번호
     private String employeeTel;
-    //    업체 전화번호
-    private String customerTel;
     //    비고
     private String stocktakingNote;
 
--- backend/src/main/java/com/example/backend/mapper/stock/stocktaking/StocktakingMapper.java
+++ backend/src/main/java/com/example/backend/mapper/stock/stocktaking/StocktakingMapper.java
@@ -16,8 +16,20 @@ public interface StocktakingMapper {
     List<Stocktaking> list();
 
     @Select("""
-            SELECT *
-            FROM TB_STKTK
+            SELECT s.stocktaking_key,
+                   s.item_code,
+                   s.warehouse_code,
+                   s.location_key,
+                   s.customer_employee_no,
+                   s.count_current,
+                   s.count_configuration,
+                   s.stocking_date,
+                   w.warehouse_name,
+                   w.customer_code,
+                   itcm.item_common_name itemName
+            FROM TB_STKTK s 
+                LEFT JOIN TB_WHMST w ON s.warehouse_code=w.warehouse_code
+                LEFT JOIN TB_ITEMCOMM itcm ON s.item_code=itcm.item_common_code
             WHERE stocktaking_key=#{stocktakingKey}
             """)
     Stocktaking view(Integer stocktakingKey);
