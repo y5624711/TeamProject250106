a34fcb636467c4a162222bd01efd499f3b1ff76b
Jin19121 <jinkim19121@gmail.com>
2025-01-24 09:11:30 +0900

    0124 김지민 fix: > 에러 해결

--- frontend/src/page/state/instk/Instk.jsx
+++ frontend/src/page/state/instk/Instk.jsx
@@ -9,7 +9,7 @@ export function Instk() {
       <StateSideBar />
       <Stack flex={1} p={5}>
         <Heading size={"xl"} p={2} mb={3}>
-          구매 / 설치관리 > 입고 관리
+          구매 / 설치관리 {">"} 입고 관리
         </Heading>
         <InstkList />
       </Stack>
