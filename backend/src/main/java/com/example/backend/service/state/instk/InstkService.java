914aec72e85e21a687033f04c5cadf563ec51888
Jin19121 <jinkim19121@gmail.com>
2025-01-25 17:35:26 +0900

    0125 김지민 comment: INSTK 오류로 각주처리

--- backend/src/main/java/com/example/backend/service/state/instk/InstkService.java
+++ backend/src/main/java/com/example/backend/service/state/instk/InstkService.java
@@ -13,33 +13,33 @@ import java.util.List;
 @RequiredArgsConstructor
 public class InstkService {
     final InstkMapper mapper;
-    public List<Instk> viewlist() {
-
-         List<Instk> instkList = mapper.viewBuyInList();
-
-        return  instkList;
-
-    }
+//    public List<Instk> viewlist() {
+//
+//         List<Instk> instkList = mapper.viewBuyInList();
+//
+//        return  instkList;
+//
+//    }
 
     public Instk detailView(int inputKey) {
 
 
         //시리얼 번호 목록 
-       List<Integer> serialList= mapper.getSerialNoByInputKey(inputKey);
+        List<Integer> serialList = mapper.getSerialNoByInputKey(inputKey);
 
         System.out.println("serialList = " + serialList);
         //  입고시 비고내용
-         String inputStockNote= mapper.getInstkNoteByInputKey(inputKey,serialList.get(0));
+        String inputStockNote = mapper.getInstkNoteByInputKey(inputKey, serialList.get(0));
 
 
 //
         System.out.println("inputStockNote = " + inputStockNote);
 
-      Instk instk = new Instk();
-      instk.setInputNote(inputStockNote);
-      instk.setSerialList(serialList);
+        Instk instk = new Instk();
+        instk.setInputNote(inputStockNote);
+        instk.setSerialList(serialList);
         System.out.println("instk = " + instk);
-      return instk;
+        return instk;
 
     }
 }
