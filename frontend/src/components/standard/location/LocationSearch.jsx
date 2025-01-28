067131b177171d31db5b363a478e993d8dc1b805
hp <agaapemin@gmail.com>
2025-01-29 02:26:49 +0900

    0128 민재원 fix: warehouse, location 검색바

--- frontend/src/components/standard/location/LocationSearch.jsx
+++ frontend/src/components/standard/location/LocationSearch.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import {
-  Box,
+  HStack,
   Input,
   SelectContent,
   SelectItem,
   SelectRoot,
   SelectTrigger,
   SelectValueText,
-  Stack,
 } from "@chakra-ui/react";
 import { Button } from "../../ui/button.jsx";
 
@@ -18,45 +17,44 @@ function LocationSearch({
   handleSearchClick,
 }) {
   return (
-    <Box>
-      <Stack justify={"top"} direction={"row"}>
-        <Box>
-          <SelectRoot
-            collection={locationOptionList}
-            defaultValue={["all"]}
-            width="120px"
-            onValueChange={(oc) => setSearch({ ...search, type: oc.value })}
-          >
-            <SelectTrigger>
-              <SelectValueText />
-            </SelectTrigger>
-            <SelectContent>
-              {locationOptionList.items.map((option) => (
-                <SelectItem item={option} key={option.value}>
-                  {option.label}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </SelectRoot>
-        </Box>
+    <HStack justifyContent="center">
+      <SelectRoot
+        collection={locationOptionList}
+        defaultValue={["all"]}
+        position="relative"
+        width="160px"
+        onValueChange={(oc) => setSearch({ ...search, type: oc.value })}
+      >
+        <SelectTrigger>
+          <SelectValueText />
+        </SelectTrigger>
+        <SelectContent
+          style={{
+            width: "150px",
+            top: "40px",
+            position: "absolute",
+          }}
+        >
+          {locationOptionList.items.map((option) => (
+            <SelectItem item={option} key={option.value}>
+              {option.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </SelectRoot>
 
-        <Box>
-          <Input
-            placeholder="키워드를 입력해주세요"
-            width="700px"
-            onChange={(e) => setSearch({ ...search, keyword: e.target.value })}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                handleSearchClick();
-              }
-            }}
-          ></Input>
-        </Box>
-        <Box>
-          <Button onClick={handleSearchClick}>검색</Button>
-        </Box>
-      </Stack>
-    </Box>
+      <Input
+        placeholder="검색어를 입력해 주세요."
+        width="50%"
+        onChange={(e) => setSearch({ ...search, keyword: e.target.value })}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSearchClick();
+          }
+        }}
+      ></Input>
+      <Button onClick={handleSearchClick}>검색</Button>
+    </HStack>
   );
 }
 
