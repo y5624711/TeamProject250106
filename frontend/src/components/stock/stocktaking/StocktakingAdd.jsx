bddf825a4e6fe8fc4e872bed847553be6dff6c5c
hp <agaapemin@gmail.com>
2025-01-31 03:05:32 +0900

    0130 민재원 feat: 실사 등록

--- frontend/src/components/stock/stocktaking/StocktakingAdd.jsx
+++ frontend/src/components/stock/stocktaking/StocktakingAdd.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   DialogActionTrigger,
   DialogBody,
@@ -13,23 +13,25 @@ import { Box, Input } from "@chakra-ui/react";
 import { Button } from "../../ui/button.jsx";
 import axios from "axios";
 import { toaster } from "../../ui/toaster.jsx";
+import { Field } from "../../ui/field.jsx";
 
 function StocktakingAdd({ isOpen, onConfirm, onClose, title }) {
+  const [warehouseCode, setWarehouseCode] = useState("");
+  const [itemCode, setItemCode] = useState("");
+  const [countCurrent, setCountCurrent] = useState("");
+  const [countConfiguration, setCountConfiguration] = useState("");
+  const [customerEmployeeNo, setCustomerEmployeeNo] = useState("");
+  const [stocktakingNote, setStocktakingNote] = useState();
+
   const handleSaveClick = () => {
     axios
       .post(`/api/stocktaking/add`, {
-        // warehouseCode,
-        // warehouseName,
-        // customerCode,
-        // warehouseAddress,
-        // warehouseAddressDetail,
-        // warehousePost,
-        // warehouseState,
-        // warehouseCity,
-        // customerEmployeeNo,
-        // warehouseTel,
-        // warehouseActive,
-        // warehouseNote,
+        warehouseCode,
+        itemCommonCode,
+        countCurrent,
+        countConfiguration,
+        customerEmployeeNo,
+        stocktakingNote,
       })
       .then((res) => res.data)
       .then((data) => {
@@ -45,20 +47,59 @@ function StocktakingAdd({ isOpen, onConfirm, onClose, title }) {
   };
 
   return (
-    <DialogRoot open={isOpen} onOpenChange={onClose}>
+    <DialogRoot open={isOpen} onOpenChange={onClose} size="lg">
       <DialogContent>
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
         </DialogHeader>
-        <DialogBody>
-          <Box>
-            창고 코드 (후에 자동생성으로 변경)
+        <DialogBody
+          style={{ display: "flex", flexDirection: "column", gap: "15px" }}
+        >
+          <Field label="창고 코드" orientation="horizontal" mb={15}>
             <Input
               type={"text"}
-              // value={warehouseCode}
-              // onChange={(e) => setWarehouseCode(e.target.value)}
+              value={warehouseCode}
+              onChange={(e) => setWarehouseCode(e.target.value)}
             />
+          </Field>
+          <Field label="품목 코드" orientation="horizontal" mb={15}>
+            <Input
+              type={"text"}
+              value={itemCode}
+              onChange={(e) => setItemCode(e.target.value)}
+            />
+          </Field>
+          <Box display="flex" gap={4}>
+            <Field label="전산 수량" orientation="horizontal" mb={15}>
+              <Input
+                type={"text"}
+                value={countCurrent}
+                onChange={(e) => setCountCurrent(e.target.value)}
+              />
+            </Field>
+            <Field label="실제 수량" orientation="horizontal" mb={15}>
+              <Input
+                type={"text"}
+                value={countConfiguration}
+                onChange={(e) => setCountConfiguration(e.target.value)}
+              />
+            </Field>
           </Box>
+          <Field label="담당자 사번" orientation="horizontal" mb={15}>
+            <Input
+              type={"text"}
+              value={customerEmployeeNo}
+              onChange={(e) => setCustomerEmployeeNo(e.target.value)}
+            />
+          </Field>
+          <Field label="비고" orientation="horizontal" mb={15}>
+            <Input
+              type={"text"}
+              value={stocktakingNote}
+              onChange={(e) => setStocktakingNote(e.target.value)}
+            />
+          </Field>
+          {/*  TODO: 실사유형 radio로 체크  */}
         </DialogBody>
         <DialogFooter>
           <DialogCloseTrigger onClick={onClose} />
