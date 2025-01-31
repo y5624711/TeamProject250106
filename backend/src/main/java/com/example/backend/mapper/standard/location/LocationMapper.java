62fe426cef6eef3d149f425035a81c2019e15996
Lemonstew <colt4949@naver.com>
2025-01-31 13:02:39 +0900

    0131 민재원 fix: 로케이션 컬럼 삭제

--- backend/src/main/java/com/example/backend/mapper/standard/location/LocationMapper.java
+++ backend/src/main/java/com/example/backend/mapper/standard/location/LocationMapper.java
@@ -19,22 +19,17 @@ public interface LocationMapper {
                 l.col,
                 l.shelf,
                 l.location_key,
-                l.item_common_code,
                 l.location_note,
-                w.warehouse_name,
-                itc.item_common_name
+                w.warehouse_name
             FROM TB_LOCMST l
             LEFT JOIN TB_WHMST w ON l.warehouse_code=w.warehouse_code
-            LEFT JOIN TB_ITEMCOMM itc ON l.item_common_code=itc.item_common_code
             WHERE 
                     <if test="searchType == 'all'">
                         l.warehouse_code LIKE CONCAT('%',#{searchKeyword},'%')
                      OR w.warehouse_name LIKE CONCAT('%',#{searchKeyword},'%')
-                     OR itc.item_common_name LIKE CONCAT('%',#{searchKeyword},'%')
                      OR l.row LIKE CONCAT('%',#{searchKeyword},'%')
                      OR l.col LIKE CONCAT('%',#{searchKeyword},'%')
                      OR l.shelf LIKE CONCAT('%',#{searchKeyword},'%')
-                     OR l.item_common_code LIKE CONCAT('%',#{searchKeyword},'%')
                      OR l.location_note LIKE CONCAT('%',#{searchKeyword},'%')
                     </if>
                     <if test="searchType != 'all'">
@@ -51,10 +46,6 @@ public interface LocationMapper {
                              </when>
                              <when test="searchType == 'shelf'">
                                  l.shelf LIKE CONCAT('%', #{searchKeyword}, '%')
-                             </when>
-                             <when test="searchType == 'item'">
-                                 l.item_common_code LIKE CONCAT('%', #{searchKeyword}, '%')
-                              OR itc.item_common_name LIKE CONCAT('%',#{searchKeyword},'%')
                              </when>
                              <when test="searchType == 'note'">
                                  l.location_note LIKE CONCAT('%', #{searchKeyword}, '%')
@@ -78,8 +69,8 @@ public interface LocationMapper {
     List<Location> list(String searchType, String searchKeyword, Integer pageList, String sort, String order);
 
     @Insert("""
-            INSERT INTO TB_LOCMST (warehouse_code, row, col, shelf, item_common_code, location_note)
-            VALUES ( #{warehouseCode}, #{row}, #{col}, #{shelf}, #{itemCommonCode}, #{locationNote} )
+            INSERT INTO TB_LOCMST (warehouse_code, row, col, shelf, location_note)
+            VALUES ( #{warehouseCode}, #{row}, #{col}, #{shelf}, #{locationNote} )
             """)
     int add(Location location);
 
@@ -88,16 +79,13 @@ public interface LocationMapper {
             SELECT COUNT(*)
             FROM TB_LOCMST l
             LEFT JOIN TB_WHMST w ON l.warehouse_code=w.warehouse_code
-            LEFT JOIN TB_ITEMCOMM itc ON l.item_common_code=itc.item_common_code
             WHERE 
                     <if test="searchType == 'all'">
                         l.warehouse_code LIKE CONCAT('%',#{searchKeyword},'%')
                      OR w.warehouse_name LIKE CONCAT('%',#{searchKeyword},'%')
-                     OR itc.item_common_name LIKE CONCAT('%',#{searchKeyword},'%')
                      OR l.row LIKE CONCAT('%',#{searchKeyword},'%')
                      OR l.col LIKE CONCAT('%',#{searchKeyword},'%')
                      OR l.shelf LIKE CONCAT('%',#{searchKeyword},'%')
-                     OR l.item_common_code LIKE CONCAT('%',#{searchKeyword},'%')
                      OR l.location_note LIKE CONCAT('%',#{searchKeyword},'%')
                     </if>
                     <if test="searchType != 'all'">
@@ -114,10 +102,6 @@ public interface LocationMapper {
                              </when>
                              <when test="searchType == 'shelf'">
                                  l.shelf LIKE CONCAT('%', #{searchKeyword}, '%')
-                             </when>
-                             <when test="searchType == 'item'">
-                                 l.item_common_code LIKE CONCAT('%', #{searchKeyword}, '%')
-                              OR itc.item_common_name LIKE CONCAT('%',#{searchKeyword},'%')
                              </when>
                              <when test="searchType == 'note'">
                                  l.location_note LIKE CONCAT('%', #{searchKeyword}, '%')
@@ -139,13 +123,10 @@ public interface LocationMapper {
                 l.col,
                 l.shelf,
                 l.location_key,
-                l.item_common_code,
                 l.location_note,
-                w.warehouse_name,
-                itc.item_common_name
+                w.warehouse_name
             FROM TB_LOCMST l
             LEFT JOIN TB_WHMST w ON l.warehouse_code=w.warehouse_code
-            LEFT JOIN TB_ITEMCOMM itc ON l.item_common_code=itc.item_common_code
             WHERE l.location_key=#{locationKey}
             </script>
             """)
@@ -153,7 +134,7 @@ public interface LocationMapper {
 
     @Update("""
             UPDATE TB_LOCMST
-            SET warehouse_code=#{warehouseCode},item_common_code=#{itemCommonCode}, location_note=#{locationNote}
+            SET warehouse_code=#{warehouseCode}, location_note=#{locationNote}
             WHERE location_key=#{locationKey}
             """)
     int edit(Location location);
