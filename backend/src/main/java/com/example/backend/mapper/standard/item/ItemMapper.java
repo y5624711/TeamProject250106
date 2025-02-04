9abc6e11842549a03edd4734de56545480f1c91c
qortn4925 <qortn4925@naver.com>
2025-02-04 18:14:00 +0900

    02/04 김용수 feat: 입고 정수값으로 가져오도록 해결

--- backend/src/main/java/com/example/backend/mapper/standard/item/ItemMapper.java
+++ backend/src/main/java/com/example/backend/mapper/standard/item/ItemMapper.java
@@ -144,7 +144,7 @@ public interface ItemMapper {
     List<String> getUsedItemCommonCode();
 
 
-    //    코드중 시리얼 넘버 최댓값 가져오기
+    //    코드중 시리얼 넘버 최댓값 가져오기 > 정수값으로 변환하도록
     @Select("""
     SELECT MAX(CAST(SUBSTRING(serial_no, 2) AS UNSIGNED))
     FROM TB_ITEMSUB
