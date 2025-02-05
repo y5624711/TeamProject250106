f1dd85d57bb40fa6c66a455950677b3f31d1c4f4
qortn4925 <qortn4925@naver.com>
2025-02-05 15:34:40 +0900

    02/05 김용수 feat:반려 상세 모달창 띄우는거 변경

--- frontend/src/components/state/instk/InstkDetaiViewModal.jsx
+++ frontend/src/components/state/instk/InstkDetaiViewModal.jsx
@@ -204,7 +204,10 @@ export function InstkDetaiViewModal({
                   <Input readOnly value={detailData.disapproveDate} />
                 </Field>
               )}
+
+              {/*true고  비고 없으면 필드 , */}
               {instk.inputConsent === true ? (
+                // true
                 <Field label={"승인 비고"} orientation="horizontal">
                   {instk.inputStockNote ? (
                     <Textarea
@@ -217,8 +220,17 @@ export function InstkDetaiViewModal({
                   )}
                 </Field>
               ) : (
+                // false
                 <Field label={"반려 비고"} orientation="horizontal">
-                  <Input readOnly value={detailData.disapproveNote} />
+                  {instk.inputStockNote ? (
+                    <Textarea
+                      readOnly
+                      value={instk.inputStockNote}
+                      style={{ maxHeight: "100px", overflowY: "auto" }}
+                    />
+                  ) : (
+                    <Input readOnly value={"내용 없음"} />
+                  )}
                 </Field>
               )}
             </Stack>
