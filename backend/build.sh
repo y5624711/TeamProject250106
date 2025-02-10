43fefe6efe31bf35e85fd3eca574383a5016a433
Yun <y5624711@gmail.com>
2025-02-10 15:00:23 +0900

    0210 윤병관 feat. 프로젝트 빌드파일 추가

--- backend/build.sh
+++ backend/build.sh
@@ -11,3 +11,4 @@ cd ../backend
 
 scp -i src/main/resources/secret/key1226.pem build/libs/backend-0.0.1-SNAPSHOT.jar ubuntu@54.180.144.145:./prj.jar
 ssh -i src/main/resources/secret/key1226.pem ubuntu@54.180.144.145 './run.sh'
+#
